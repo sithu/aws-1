cad3159224b0a1c08bc2deab7123a72476463484
John Whitton <john.a.whitton@gmail.com>
2016-05-17 08:49:27 -0700

    Open up parity to everyone with --rpccorsdomain "*"
    Need to work out how to limit this moving forward

--- paritydev.sh
+++ paritydev.sh
@@ -27,7 +27,7 @@ parity account list
 # Also created 67f726a1d40f2e34fedb2e7aefe2214e722dd948 and c6efb86f0d7ccf6b188fee08e5ed6f37871b947a
 
 # Run Parity in foreground
-parity --testnet --unlock faa5f4159c732b38b755a742aff2bf59218c50b5 --password "./password.txt" --pruning "fast" --force-sealing --rpc --maxpeers "100" --cache "3062" --author "70e84041de436631508099b3f2504315cb31a907"  --webapp-port "8080" --webapp-interface "all" --webapp-user "admin" --webapp-pass "admin" --jsonrpc-interface "all" -jw
+parity --testnet --unlock faa5f4159c732b38b755a742aff2bf59218c50b5 --password "./password.txt" --pruning "fast" --force-sealing --rpc --maxpeers "100" --cache "3062" --author "70e84041de436631508099b3f2504315cb31a907"  --webapp-port "8080" --webapp-interface "all" --webapp-user "admin" --webapp-pass "admin" --jsonrpc-interface "all" --rpccorsdomain "*" -jw
 # Test it by going to http://54.191.51.165:8080/ in a browser to see the admin console
 # Test mining by running the following locally ethminer -G --opencl-device 1 -F "http://54.191.51.165:8545"
 # Note mining will be unable to get a package until block synchs
@@ -36,4 +36,4 @@ parity --testnet --unlock faa5f4159c732b38b755a742aff2bf59218c50b5 --password ".
 # Run the parity unlocking an account and using testnet (notice the nohup with an & at the end will run it in background)
 # note you can view the jobs with a ps -ef | grep parity
 # you can kill the job using kill -9 2806 (where 2806 is the pid you got whenn you did your ps -ef | grep parity)
-nohup parity --testnet --unlock faa5f4159c732b38b755a742aff2bf59218c50b5 --password "./password.txt" --pruning "fast" --force-sealing --rpc --maxpeers "100" --cache "3062" --author "70e84041de436631508099b3f2504315cb31a907"  --webapp-port "8080" --webapp-interface "all" --webapp-user "admin" --webapp-pass "admin" --jsonrpc-interface "all" -jw > /dev/null &
+nohup parity --testnet --unlock faa5f4159c732b38b755a742aff2bf59218c50b5 --password "./password.txt" --pruning "fast" --force-sealing --rpc --maxpeers "100" --cache "3062" --author "70e84041de436631508099b3f2504315cb31a907"  --webapp-port "8080" --webapp-interface "all" --webapp-user "admin" --webapp-pass "admin" --jsonrpc-interface "all" --rpccorsdomain "*" -jw > /dev/null &
